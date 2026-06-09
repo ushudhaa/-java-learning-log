65ae9a5953f3489b5103b3e39c7cec2fcb299995
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-06-09 23:40:44 +0545

    Encapsulation

---
+++ Test/test.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class test {
+    static void main(String[] args) {
+        student s = new student();
+        s.name = "Ushudha";
+        s.roll_no = 2;
+        s.setAge(-1);
+        System.out.println(s.getAge());
+    }
+}
